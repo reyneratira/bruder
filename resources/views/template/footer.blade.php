150310ad0bc561ad7bfbef5c47cac1e9a4224fa5
rfdzhifa <rfdnazhifah@gmail.com>
2023-01-24 08:41:04 +0700

    add footer

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -12,13 +12,12 @@
       new WOW().init();
     </script> -->
 
- 
-<div class="container my-5">
+{{-- <div class="container my-5">
 
-<footer class="text-center text-white" style="background-color: #f1f1f1;">
+<footer class="text-center text-white" style="background-color: #f1f1f1; width: 100%">
 <!-- Grid container -->
 <div class="container pt-4">
-  <!-- Section: Social media -->
+  <!-- Section: Social media -->`
   <section class="mb-4">
     <!-- Facebook -->
     <a
@@ -51,5 +50,22 @@
 <!-- Copyright -->
 </footer>
 
+</div> --}}
+
+<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+      </a>
+      <span class="mb-3 mb-md-0 text-muted">© 2022 Kelompok 1, RPL 6 </span>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li class="ms-3"><a class="text-muted" href="#"><img class="bi" width="24" height="24" src="assets/image/facebook.png"></img></a></li>
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"></svg></a></li>
+      <li class="ms-3"><a class="text-muted" href="#"><img class="bi" width="24" height="24" src="assets/image/instagram.png"></img></a></li>
+    </ul>
+  </footer>
 </div>
 <!-- End of .container -->
